87c1fc39a88946ca662f7029e570784cb41d91e6
srosales4 <35977237+srosales4@users.noreply.github.com>
2018-09-13 21:59:25 -0600

    Modify p4 version of shell
    I was trying to figure out shifting between parent and child.  The parent would loop a prompt for input and the child would execute the command.  Forking still not entirely solved - on one test case I would get two write outs, eg., "$$" and "Parent: My pid.. Parent: My pid." Still have yet to print out results to my command: "wc shell.py"

--- shell/shell.py
+++ shell/shell.py
@@ -7,6 +7,7 @@
 # To get another viewpoint on the topics of fork() and exec() read:
 
 import os, sys, time, re
+from wordCount import tokenize
 
 pid = os.getpid()               # get and remember pid
 
@@ -18,21 +19,20 @@ if rc < 0:
     os.write(2, ("fork failed, returning %d\n" % rc).encode())
     sys.exit(1)
 
-elif rc == 0:                   # child
-    
-    while True:
-        os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
-                     (os.getpid(), pid)).encode())
-        args = input('minersh:: ')
-        tokens = args.split(' ')
+elif rc == 0:
 
-        for dir in re.split(':', os.environ['PATH']): # try each directory in path
-            program = "%s/%s" % (dir, tokens[0])
-            try:
-                os.execve(program, tokens[1:], os.environ) # try to exec program
-            except OSError as e:                      # ...expected
-                os.write(2, ("%s\n" % e).encode() )
-                pass                                       # ...fail quietly
+    os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
+                 (os.getpid(), pid)).encode())
+
+    for dir in re.split(':', os.environ['PATH']): # try each directory in path
+        program = "%s/%s" % (dir, tokens[0])
+        os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
+        try:
+            os.execve(program, tokens[1:], os.environ) # try to exec program
+            os.wait()
+        except OSError as e:                      # ...expected
+            os.write(2, ("%s\n" % e).encode() )
+            pass                                       # ...fail quietly
 
         # for dir in re.split(":", os.environ['PATH']): # try each directory in path
         #     program = "%s/%s" % (dir, tokens[0])
@@ -58,8 +58,19 @@ elif rc == 0:                   # child
     #sys.exit(1)                 # terminate with error
 
 else:                           # parent (forked ok)
-    os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
-                 (pid, rc)).encode())
     childPidCode = os.wait()
+
+    command = ""
+    while True:
+        os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
+                 (pid, rc)).encode())
+        command = input('$ ')
+        tokens = tokenize(' ', command)
+
+        if tokens[0] == '':
+            break
+        os.write(1, ("%s\n" % tokens).encode() )
+        os.fork()
+
     os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
                  childPidCode).encode())
