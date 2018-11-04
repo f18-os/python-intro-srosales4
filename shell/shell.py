e3ba23d04940bab4f907d9fc8e5f54d137fb86a8
Checo Ponce De Leon <checopdleon@gmail.com>
2018-11-04 10:45:25 -0700

    Modularize the shell. All helper methods in shell/utilities.py

--- shell/shell.py
+++ shell/shell.py
@@ -1,14 +1,27 @@
-import os, sys
+#! /usr/bin/env python3
 
-command = ""
+import os, sys, time, re
+from wordCount import tokenize
+from utilities import runProgram
 
-while (command != "exit"):
-	command = input('minersh::\n')
-	userInput = command.split(" ")
+""" The following is based on:
+	-) https://github.com/robustUTEP/os-demos/blob/master/ch5-api/p3-exec.py
+	-) https://brennan.io/2015/01/16/write-a-shell-in-c/
+"""
 
-	if ".py" in userInput[0]:
-		program = userInput[0]
-		io = 'python3 %s %s %s' % (program, userInput[1], userInput[2])
-		os.system(io)
-	elif "." not in userInput[0]:
-		os.system(command)
+# set the status an impossibly large number as an initial start
+status = sys.maxsize
+args = ''
+
+while True:
+	args = input("$ ").split(' ')
+
+	if len(args) == 1 and args[0] == '':
+		os.write(2, ("No input\n").encode())
+		continue
+	else:
+		# if succesful status is set to 1
+		status = runProgram(args)
+	# if runProgram is not successful stop the show
+	if not status is 1:
+		break
---
+++ shell/utilities.py
@@ -0,0 +1,57 @@
+import argparse
+import os, sys, time, re
+
+""" The following is based on:
+	-) https://brennan.io/2015/01/16/write-a-shell-in-c/
+"""
+
+def parseArguments(commandLineCommand):
+	ap = argparse.ArgumentParser()
+	args = ap.pars_agrs()
+
+
+def runProgram(args):
+	pid = os.getpid()
+
+	os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
+	rc = os.fork()
+
+	if rc < 0:
+		os.write(2, ("fork failed, returning %d\n" % rc).encode())
+		sys.exit(-1)
+
+	elif rc == 0:
+		os.write(1, ("Child: My pid==%d. Parent's pid==%d\n" % 
+					(os.getpid(), pid)).encode())
+
+		if execProgram(args) == -1:
+			os.write(2, ("error in utilities, runProgram, rc == 0\n").encode())
+			return -1
+
+	else:
+		childPIDcode = os.wait()
+		os.write(1, ("Parent: My pid=%d. Child's pid=%d\n" %
+					(pid, rc)).encode())
+		os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
+					childPIDcode).encode())
+
+	return 1
+
+
+def execProgram(args):
+	for dir in re.split(":", os.environ['PATH']):
+		programPath = "%s/%s" %(dir, args[0])
+		os.write(1, ("runProgram: exec-ing %s\n" % programPath).encode())
+		try:
+			if len(args) == 1:
+				os.execv(programPath, args)
+			else:
+				os.execve(programPath, args[1:], os.environ)
+				os.write(1, ("next line after exec").encode())
+		except FileNotFoundError:
+			pass
+
+	# else something went wrong
+	os.write(2, ("Could not exec %s\n" % args[0]).encode())
+	# and return -1 to signal an error
+	return -1
